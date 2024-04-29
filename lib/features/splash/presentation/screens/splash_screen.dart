b1f52f05b2da8321ca278907342df9cd429b33c0
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-04-29 20:41:14 +0600

    add hooks to splash

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,42 +1,35 @@
-import 'package:firebase_chat_app/core/config/firebase/FirebaseSettings.dart';
-import 'package:firebase_chat_app/utils/AppStrings.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:go_router/go_router.dart';
+import 'package:firebase_chat_app/core/config/firebase/FirebaseSettings.dart';
 import 'package:firebase_chat_app/core/config/routing/app_router_generator.dart';
+import 'package:firebase_chat_app/utils/AppStrings.dart';
 
-final authStateProvider = StreamProvider<User?>((ref) {
-  return FirebaseAuth.instance.authStateChanges();
-});
-
-class SplashScreen extends ConsumerWidget {
+class SplashScreen extends HookWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final authData = ref.watch(authStateProvider);
+  Widget build(BuildContext context) {
+    final authData = useStream(
+      FirebaseAuth.instance.authStateChanges(),
+      initialData: null,
+    );
 
-    return Scaffold(
+    useEffect(() {
+      _handleAuthState(authData.data, context);
+      // Clean up effect
+      return () {};
+    }, [authData]); // Trigger effect only when authData changes
+
+    return const Scaffold(
       body: Center(
-        child: authData.when(
-          data: (user) => _handleToken(user, context),
-          loading: () {
-            return const Text(AppStrings.checkingUserInfo);
-          },
-          error: (error, stackTrace) {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              context.goNamed(RouteNames.login);
-            });
-
-            return const SizedBox.shrink();
-          },
-        ),
+        child: SizedBox.shrink(), // Show a loading indicator
       ),
     );
   }
 
-  Widget _handleToken(User? user, BuildContext context) {
+  void _handleAuthState(User? user, BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (user == null) {
         context.goNamed(RouteNames.login);
@@ -48,7 +41,5 @@ class SplashScreen extends ConsumerWidget {
         }
       }
     });
-
-    return const SizedBox.shrink();
   }
 }
